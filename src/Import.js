8be0a895fedf22ded5d79c43809251693ae0b599
Brian4755 <brian.mccune1@yahoo.com>
2024-01-24 10:45:00 -0600

    drag and drop importfile

--- src/Import.js
+++ src/Import.js
@@ -1,9 +1,27 @@
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
 const Import = () => {
+  const onDrop = useCallback(acceptedFiles => {
+    console.log(acceptedFiles)
+  },[])
+
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
   return ( 
     <div className="document-file-import">
       Select a manifest that you'd like to import
-      <br />
-      <input className='file' type='file' />
+      <div className="drag-container">
+        <div {...getRootProps()} className="drag-n-drop">
+          <input {...getInputProps()} />
+          {
+            isDragActive ?
+              <p>Drop the files here ...</p> :
+              <p>Drag & Drop Here Or <span>Browse</span></p>
+          }
+        </div>
+        <button>Upload manifest</button>
+      </div>
     </div>
    );
 }
